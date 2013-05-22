818717cbcfac11544a1318facd14a35b5a09514f
DAIANE AZEVEDO DE FRAGA <07109812@l1544600.portoalegre.pucrsnet.br>
2013-05-21 22:19:52 -0300

    disp

--- displayer.vhd
+++ displayer.vhd
@@ -12,16 +12,23 @@ library ieee;
 entity displayer is
     port
     (
-		clk_in		: in	std_logic;
-    	data_in		: in	std_logic_vector(4 downto 0);
+        clk_in		: in	std_logic;
+        data_in		: in	std_logic_vector(4 downto 0);
         num_out     : out   std_logic_vector(3 downto 0);	
         en0_out		: out	std_logic;	-- enable for unit display
         en1_out		: out	std_logic	-- enable for ten display
     );
 end displayer;
-
+--
+-- Exemplo: data_in = 10101 (21 decimal)
+--          data_in = 11011 (27 decimal)
+--
+--
 architecture displayer of displayer is
 
+    signal  unit    : std_logic_vector(3 downto 0);
+    signal  ten     : std_logic_vector(3 downto 0);
 begin
+
 
 end displayer;
