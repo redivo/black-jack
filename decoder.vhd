a3ba51117a7e0b82881af72b98fd756ea2fd8b8b
Daiane Fraga <daiane.a.fraga@gmail.com>
2013-06-26 20:35:52 -0300

    Fix decoder

--- decoder.vhd
+++ decoder.vhd
@@ -21,18 +21,19 @@ architecture decoder of decoder is
 
     signal  num_int : std_logic_vector(3 downto 0);
 begin
-    num_int <= num_in;
-														-- 0 up to 9
-    bcd_out <= "11111100" when num_int = "0000" else  -- 0
-               "01100000" when num_int = "0001" else  -- 1
-               "11011010" when num_int = "0010" else  -- 2
-               "11110010" when num_int = "0011" else  -- 3
-               "01100110" when num_int = "0100" else  -- 4
-               "10110110" when num_int = "0101" else  -- 5
-               "01001001" when num_int = "0110" else  -- 6
-               "11100000" when num_int = "0111" else  -- 7
-               "11111110" when num_int = "1000" else  -- 8
-               "11110110" when num_int = "1001" else	-- 9
-				       "00000000";	-- default
+	num_int <= num_in;
+				
+				-- 0 up to 9
+	bcd_out <=	"11111100" when num_int = "0000" else	-- 0
+				"01100000" when num_int = "0001" else	-- 1
+				"11011010" when num_int = "0010" else	-- 2
+				"11110010" when num_int = "0011" else	-- 3
+				"01100110" when num_int = "0100" else	-- 4
+				"10110110" when num_int = "0101" else	-- 5
+				"10111110" when num_int = "0110" else	-- 6
+				"11100000" when num_int = "0111" else	-- 7
+				"11111110" when num_int = "1000" else	-- 8
+				"11110110" when num_int = "1001" else	-- 9
+				"00000000";								-- default
 end decoder;
 
